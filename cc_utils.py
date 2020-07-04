8aeb295b794a2451d1f3d63ade5f858a0468121c
Boris Efraty <efratyb@gmail.com>
2020-07-05 00:40:35 +0300

    added files

---
+++ cc_utils.py
@@ -0,0 +1,137 @@
+import numpy as np
+import pandas as pd
+import cv2
+from numpy import genfromtxt
+from sklearn.cross_decomposition import PLSRegression
+
+# def ApplyRGBTransform(rgb,BT):
+#     # faster method
+#     B = BT[0]
+#     result = rgb + B
+#     return result
+
+def ApplyRGBTransform(rgb,BT):
+    # faster method
+    B = BT[0]
+    M = BT[1]
+    rgb_reshaped = rgb.reshape((rgb.shape[0] * rgb.shape[1], rgb.shape[2]))
+    result = np.dot(M, rgb_reshaped.T).T.reshape(rgb.shape)
+    result = result + B
+    result = np.clip(result,0,255)
+    return result
+
+def ApplyRGBTransformPLS(org_im,pls):
+    # faster method
+    calib_img = org_im.copy()
+    for im in calib_img:
+        im[:] = pls.predict(im[:])
+    return calib_img
+
+
+def FindRGBTransformPLS(rgbFrom, rgbTo):
+    pls = PLSRegression(n_components=3)
+    pls.fit(rgbFrom, rgbTo)
+    sc = pls.score(rgbFrom, rgbTo)
+    print(sc)
+    return pls
+
+def FindRGBTransform(rgbFrom, rgbTo, bias = True, transf = True):
+    #TODO
+  #  rgbFrom = rgbFrom[:,::-1]
+
+
+    B = [0,0,0]
+    T = np.eye(3)
+
+    if(bias):
+        mFrom = np.mean(rgbFrom,0)
+        mTo = np.mean(rgbTo, 0)
+        B = mTo - mFrom
+    if(transf):
+        rgbFrom_centered = rgbFrom - np.mean(rgbFrom, 0)
+        rgbTo_centered = rgbTo - np.mean(rgbTo, 0)
+        temp = np.linalg.pinv(rgbFrom_centered)
+        T = np.dot(temp, rgbTo_centered)
+        B = np.mean(rgbTo, 0) - np.dot(np.mean(rgbFrom, 0),T)
+
+
+
+    return [B,T]
+
+
+def FindRGBTransformWLS(rgbFrom, rgbTo, bias = True, transf = True):
+    #TODO
+
+    w = np.ones(24, dtype=float)
+    w[18:] = 4.0 # gray values
+    w[18] = 24.0 # white is most important
+
+    nw = w / w.sum()  # normalized
+    NW = np.diag(nw)
+    W = np.sqrt(np.diag(nw))
+
+    B = [0,0,0]
+    T = np.eye(3)
+
+    if(bias):
+        mFrom = np.sum(np.dot(NW,rgbFrom),0)
+        mTo = np.sum(np.dot(NW,rgbTo),0)
+        B = mTo - mFrom
+    if(transf and bias):
+
+        rgbFrom_centered_ww = rgbFrom - np.sum(np.dot(NW,rgbFrom),0)
+        rgbTo_centered_ww = rgbTo - np.sum(np.dot(NW,rgbTo),0)
+        # temp = np.linalg.pinv(rgbFrom_centered)
+        # T = np.dot(temp, rgbTo_centered)
+        W = np.sqrt(np.diag(w))
+        Aw = np.dot(W, rgbFrom_centered_ww)
+        Bw = np.dot(W,rgbTo_centered_ww)
+        TT = np.linalg.lstsq(Aw, Bw)
+        T = TT[0]
+        B = np.sum(np.dot(NW,rgbTo),0) - np.dot(np.sum(np.dot(NW,rgbFrom),0),T)
+    if (transf and not bias):
+        rgbFrom_centered = rgbFrom - np.mean(rgbFrom, 0)
+        rgbTo_centered = rgbTo - np.mean(rgbTo, 0)
+        # temp = np.linalg.pinv(rgbFrom_centered)
+        # T = np.dot(temp, rgbTo_centered)
+
+        Aw = np.dot(W, rgbFrom)
+        Bw = np.dot(W, rgbTo)
+        TT = np.linalg.lstsq(Aw, Bw)
+        T = TT[0]
+
+
+
+
+    return [B,T]
+
+
+def resizeTo(im, w):
+	rsz = w/im.shape[1]
+	rr = int(im.shape[0]*rsz)
+	cc = int(im.shape[1]*rsz)
+	im1 = cv2.resize(im,(cc, rr), interpolation= cv2.INTER_LANCZOS4)
+	return im1, rsz
+
+
+def GetRGBs(im, ptrs, nei = 5):
+    N = len(ptrs)
+    rgbs = []
+    for p in ptrs:
+        p = p[::-1]
+        cc = im[p[0] - nei:p[0] + nei, p[1] - nei:p[1] + nei, :]
+        b = np.median(cc[:,:,0])
+        g = np.median(cc[:,:,1])
+        r = np.median(cc[:,:,2])
+        rgbs.append([r,g,b])
+    return rgbs
+
+
+def GetTemplate(ind = 0):
+    my_data = genfromtxt('template/template.csv', delimiter=',')
+    if(ind == 10):
+        my_data = genfromtxt('template/template10.csv', delimiter=',')
+    if (ind == 34):
+        my_data = genfromtxt('template/template34.csv', delimiter=',')
+    my_data = my_data[:,::-1]
+    return my_data
---
+++ detect_cc.py
@@ -0,0 +1,171 @@
+# import the necessary packages
+import time
+import cv2
+import numpy as np
+# initialize the list of reference points and boolean indicating
+# whether cropping is being performed or not
+refPt = []
+cropping = False
+from cc_utils import resizeTo, GetRGBs, GetTemplate, FindRGBTransform, FindRGBTransformPLS, ApplyRGBTransform, ApplyRGBTransformPLS, FindRGBTransformWLS
+
+NUM_COLORS = 24
+
+
+def click_and_crop(event, x, y, flags, param):
+	# grab references to the global variables
+	global refPt, cropping
+	# if the left mouse button was clicked, record the starting
+	# (x, y) coordinates and indicate that cropping is being
+	# performed
+	if event == cv2.EVENT_LBUTTONDOWN:
+		refPt = [(x, y)]
+		cropping = True
+	# check to see if the left mouse button was released
+	elif event == cv2.EVENT_LBUTTONUP:
+		# record the ending (x, y) coordinates and indicate that
+		# the cropping operation is finished
+		refPt.append((x, y))
+		cropping = False
+		# draw a rectangle around the region of interest
+		cv2.rectangle(imsmall, refPt[0], refPt[1], (0, 255, 0), 2)
+		cv2.imshow("zoom2cc", imsmall)
+
+def click_and_keep(event, x, y, flags, param):
+	# grab references to the global variables
+	global refPt
+	# if the left mouse button was clicked, record the starting
+	# (x, y) coordinates and indicate that cropping is being
+	# performed
+	if event == cv2.EVENT_LBUTTONDOWN:
+		refPt.append([x, y])
+		L = len(refPt)
+		print(L)
+		cv2.circle(roi,(x,y),10,rgbTemplate[L-1,:], -1)
+		cv2.imshow("ROI", roi)
+
+
+
+# construct the argument parser and parse the arguments
+im = "cc/00001565.tif"
+#im = "cc/00001640.tif"
+#im = "cc/00001479.tif"
+# load the image, clone it, and setup the mouse callback function
+
+image = cv2.imread(im)
+rgbTemplate = GetTemplate()
+rgbTemplate = rgbTemplate[0:NUM_COLORS,:]
+
+
+lenPtr = 0
+lastLenPtr = 0
+
+imsmall, rsz = resizeTo(image,1000)
+clone_small = imsmall.copy()
+
+cv2.namedWindow("zoom2cc")
+cv2.setMouseCallback("zoom2cc", click_and_crop)
+
+#zoom to correct region
+# keep looping until the 'q' key is pressed
+while True:
+	# display the image and wait for a keypress
+	cv2.imshow("zoom2cc", imsmall)
+	key = cv2.waitKey(1) & 0xFF
+	# if the 'r' key is pressed, reset the cropping region
+	if key == ord("r"):
+		imsmall = clone_small.copy()
+	# if the 'c' key is pressed, break from the loop
+	elif key == ord("c"):
+		break
+# if there are two reference points, then crop the region of interest
+# from teh image and display it
+if len(refPt) == 2:
+	cv2.destroyWindow("zoom2cc")
+	refPt = (np.array(refPt)/rsz).astype(int)
+	roi = image[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+	roi, rsz = resizeTo(roi,500)
+
+	cv2.imshow("ROI", roi)
+	cv2.waitKey(delay = 1)
+# close all open windows
+cv2.destroyAllWindows()
+
+
+refPt = []
+
+cv2.namedWindow("ROI")
+cv2.setMouseCallback("ROI", click_and_keep)
+clone_roi = roi.copy()
+# keep looping until the 'q' key is pressed
+while True:
+	# display the image and wait for a keypress
+	cv2.imshow("ROI", roi)
+	key = cv2.waitKey(1) & 0xFF
+	# if the 'r' key is pressed, reset the cropping region
+	if key == ord("r"):
+		roi = clone_roi.copy()
+	# if the 'c' key is pressed, break from the loop
+	elif key == ord("c"):
+		break
+	if(len(refPt) != lastLenPtr):
+		lastLenPtr = len(refPt)
+		print(len(refPt))
+	if len(refPt) == NUM_COLORS:
+
+		break
+
+cv2.destroyWindow("ROI")
+
+cv2.namedWindow("inROI")
+cv2.imshow("inROI", roi)
+# if there are two reference points, then crop the region of interest
+# from teh image and display it
+if len(refPt) == 4:
+	#TODO: generate NUM_COLORS points and show
+	for i in range(4):
+		cv2.rectangle(roi, (refPt[i][0]-3,refPt[i][1]-3), (refPt[i][0]+3,refPt[i][1]+3 ), (0, 255, 0), 2)
+	cv2.imshow("inROI", roi)
+	cv2.waitKey(0)
+
+# if len(refPt) == NUM_COLORS:
+# 	# generate NUM_COLORS points and show
+# 	for i in range(NUM_COLORS):
+# 		cv2.circle(roi, (refPt[i][0], refPt[i][1]), 10, rgbTemplate[i, :], -1)
+#
+# 	print(refPt)
+# 	cv2.imshow("inROI", roi)
+# 	cv2.waitKey(0)
+# # close all open windows
+cv2.destroyAllWindows()
+
+
+
+#select NUM_COLORS locations
+
+#pick colors
+RGB24 = GetRGBs(clone_roi,refPt)
+print("RGBs")
+print(RGB24)
+RGB24 = np.array(RGB24)
+RGB24 = RGB24[:,::-1]
+#find transform
+#T = FindRGBTransform(rgbFrom=RGB24, rgbTo= rgbTemplate)
+T= FindRGBTransformWLS(rgbFrom=RGB24, rgbTo= rgbTemplate)
+#tranform orig image
+newIm = ApplyRGBTransform(image, T)
+
+newIm = cv2.convertScaleAbs(newIm)
+
+cv2.namedWindow("NEW")
+cv2.imshow("NEW", resizeTo(newIm,1000)[0])
+cv2.namedWindow("OLD")
+cv2.imshow("OLD", resizeTo(image,1000)[0])
+
+cv2.imwrite("res.png",newIm)
+
+cv2.waitKey(0)
+
+print("OK")
+
+#see also
+#https://blog.francium.tech/using-machine-learning-for-color-calibration-with-a-color-checker-d9f0895eafdb
